a5f179421ab0d3ee95785076408530aa5743cd27
JZYFC <jiangzhenyu789@gmail.com>
2023-06-02 16:43:36 +0800

    Fix keymap

--- lua/keymap/custom.lua
+++ lua/keymap/custom.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 local keymap = vim.keymap
 
-opts= { silent= true, nowait = true }
+local opts= { silent= true, nowait = true }
 -- ---- Insert Mode ----
 
 
@@ -13,8 +13,8 @@ keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
 keymap.set("n", "<leader>sh", ":split<CR>", opts)
 -- Bufferline
 keymap.set("n", "<space>bc", ":bp<bar>sp<bar>bn<bar>bd<CR>", opts)
-keymap.set("n", "<c-L>", ":BufferLineCycleNext<CR>", opts)
-keymap.set("n", "<c-H>", ":BufferLineCyclePrev<CR>", opts)
+keymap.set("n", "<c-s-L>", ":BufferLineCycleNext<CR>", opts)
+keymap.set("n", "<c-s-H>", ":BufferLineCyclePrev<CR>", opts)
 
 -- ---- Visual Mode ----
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
